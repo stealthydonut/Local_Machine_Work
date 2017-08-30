63ea7594595d3a5666ab0b0ba4c061f91038239e
stealthydonut <kinger6688@gmail.com>
2017-08-30 11:52:49 -0400

    Update Reserve_Daily.py

--- Reserve_Daily.py
+++ Reserve_Daily.py
@@ -1,8 +1,9 @@
 import StringIO
+import pandas as pd
 
-############################
-#Get the detail file
-############################
+#########################################
+#Get the import, export, reserve sdr file
+#########################################
 from google.cloud import storage
 client = storage.Client()
 bucket = client.get_bucket('macrofiles')
@@ -16,9 +17,28 @@ inMemoryFile.write(content)
 inMemoryFile.seek(0)
 #Note - anytime you read from a buffer you need to seek so it starts at the beginning
 #The low memory false exists because there was a lot of data
-details=pd.read_csv(inMemoryFile, low_memory=False)
+imexsdr=pd.read_csv(inMemoryFile, low_memory=False)
 
+bucket = client.get_bucket('macrofiles')
+# Then do other things...
+blob = bucket.get_blob('daily_monthly_file.csv')
+content = blob.download_as_string()
+#Because the pandas dataframe can only read from buffers or files, we need to take the string and put it into a buffer
+inMemoryFile = StringIO.StringIO()
+inMemoryFile.write(content)
+#When you buffer, the "cursor" is at the end, and when you read it, the starting position is at the end and it will not pick up anything
+inMemoryFile.seek(0)
+#Note - anytime you read from a buffer you need to seek so it starts at the beginning
+#The low memory false exists because there was a lot of data
+dailyvar=pd.read_csv(inMemoryFile, low_memory=False)
+
+#####################################################################
 #Aggregate the data at the monthly level to join with the daily files
+#####################################################################
+allgroup = imexsdr.groupby(['monthyear'], as_index=False, sort=False)['sdr_amt_mm','reserve_amt_mm','export_amt_mm','import_amt_mm'].sum() 
 
-allgroup = details.groupby(['monthyear'], as_index=False, sort=False)['sdr_amt_mm','reserve_amt_mm','export_amt_mm','import_amt_mm'].sum() 
+
+goldfile=pd.merge(dailyvar, allgroup, how='outer', left_on='monthyear', right_on='monthyear')
+
+
 
